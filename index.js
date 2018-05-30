f29d78cc37782a6cf83a3c8c130d29abff92469d
astur <astur@yandex.ru>
2018-05-30 23:14:27 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = () => {};
+const promisify = require('util').promisify;
+const http = require('http');
+const https = require('https');
+
+module.exports = (options = null) => {
+    const s = options ?
+        https.createServer(options, (request, response) => {
+            s.emit(request.url, request, response);
+        }) :
+        http.createServer((request, response) => {
+            s.emit(request.url, request, response);
+        });
+
+    s.listen = promisify(s.listen);
+    s.close = promisify(s.close);
+
+    return s;
+};
